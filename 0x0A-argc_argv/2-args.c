72777d8a332af82267ffb3bd6ce4280d0cb26497
Lala246_sh <yusufshukurah59@gmail.com>
2022-04-03 12:20:07 +0000

    laks

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives.
+ * All arguments should be printed, including the first
+ * Only print one argument per line, ending with a line
+ * @argc: number of arguments
+ * @argv: list of arguments
+ * Return: 0, success.
+ */
+int main(int argc, char *argv[])
+{
+	int iterator;
+
+	for (iterator = 0; iterator < argc; iterator++)
+	{
+		printf("a[%d]: %s\n", iterator, argv[iterator]);
+	}
+	return (0);
+}
