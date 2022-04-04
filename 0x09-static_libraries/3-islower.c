d2e1472b4a7a1ca47999152bd1c11217e14b7be8
Lala246_sh <yusufshukurah59@gmail.com>
2022-04-04 12:10:14 +0000

    lala

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase
+ * @c: var to use
+ * Return: 1, if lowercase, 0 otherwise.
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strspn - length of a sibsring.
+ * @s: string.
+ * @accept: list of chars to match s.
+ * Return: number of bytes in the initial segment.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+}
