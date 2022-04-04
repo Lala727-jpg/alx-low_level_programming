f9525954614a7c12692f2cc7dadd1017f7dfe37e
Lala246_sh <yusufshukurah59@gmail.com>
2022-04-04 13:43:27 +0000

    lala

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic characters.
+ * @c: var to use.
+ * Return: 1, if letter, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+}
---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for set of bytes.
+ * @s: var to use.
+ * @accept: containing the byte to look for.
+ * Return: point to the byte in s.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+}
