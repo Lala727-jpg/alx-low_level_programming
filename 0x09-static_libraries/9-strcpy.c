af4d0f9b199a8bd9068e820b574270f9c3d33ae1
Lala246_sh <yusufshukurah59@gmail.com>
2022-04-04 14:50:39 +0000

    lala

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src.
+ * @dest: pointer to the buffer in which we copy the str
+ * @src: string to be copied.
+ * Return: pointer to dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+}
