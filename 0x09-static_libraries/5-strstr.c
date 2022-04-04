85c1a261166aee168a8fb64acaf9769e900e57cc
Lala246_sh <yusufshukurah59@gmail.com>
2022-04-04 14:26:34 +0000

    lala

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring.
+ * @haystack: string to search in.
+ * @needle: substring.
+ * Return: pointer to thr beginning of substring.
+ * or NULL IF NOT FOUND.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+}
