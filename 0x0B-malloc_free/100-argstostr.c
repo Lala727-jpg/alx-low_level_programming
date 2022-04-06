4a2905380fdb5e2f966651fba176d80788ec12f4
Lala246_sh <yusufshukurah59@gmail.com>
2022-04-06 23:42:35 +0000

    lala

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -16,6 +16,8 @@ char *argstostr(int ac, char **av)
 	int i, j, k, len;
 	char *string;
 
+	if (ac == 0 || av == NULL)
+		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
