205bfb95ac75dd2fdd25f5f723b66f4fc49c58d9
Lala246_sh <yusufshukurah59@gmail.com>
2022-04-07 01:25:54 +0000

    lala

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -46,7 +46,7 @@ char **strtow(char *str)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < len; i++)
+	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
 		{
@@ -57,7 +57,7 @@ char **strtow(char *str)
 				if (tmp == NULL)
 					return (NULL);
 				while (start < end)
-					*tmp = str[start++];
+					*tmp++ = str[start++];
 				*tmp = '\0';
 				tree[k] = tmp - c;
 				k++;
