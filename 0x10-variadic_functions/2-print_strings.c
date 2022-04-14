c4570074ee344ae9e1d94df0e504b122327ea724
Lala246_sh <yusufshukurah59@gmail.com>
2022-04-14 16:21:34 +0000

    lala

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		if (separator == NULL)
 		{
-			printf("%s", va_arg(ap, char *));
+			printf("%d", va_arg(ap, int));
 		}
 		str = va_arg(ap, char *);
 		if (str == NULL)
