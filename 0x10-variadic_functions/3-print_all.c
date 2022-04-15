69973fba5bfe9c6b934567b2fecc81896c42fdaa
Lala246_sh <yusufshukurah59@gmail.com>
2022-04-15 03:17:08 +0000

    lala

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,7 +33,7 @@ void print_all(const char * const format, ...)
 				case 's':
 					str = va_arg(ap, char *);
 					if (!str)
-						str = "nil";
+						str = "(nil)";
 					printf("%s%s", shu, str);
 					break;
 				default:
